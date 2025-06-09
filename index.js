cfebf9e6b29c016fa4afbfcf20b542119a19af88
Santiago Paz <39577108+santiago-paz@users.noreply.github.com>
2025-06-08 21:47:49 -0300

    Add 3D transformations

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ let x = 0,
     dx = 1,
     dy = 1,
     angle = 0,
+    angleX = 0,
+    angleY = 0,
     hue = 0,
     scale = 1,
-    scaleDir = 1;
+    scaleDir = 1,
+    z = 0,
+    zDir = 1;
 
 function animate() {
     x += dx;
@@ -23,7 +27,13 @@ function animate() {
     }
 
     angle = (angle + 2) % 360;
+    angleX = (angleX + 1) % 360;
+    angleY = (angleY + 1.5) % 360;
     hue = (hue + 1) % 360;
+
+    if (z >= 50) zDir = -1;
+    if (z <= -50) zDir = 1;
+    z += zDir * 0.5;
 
     if (scale >= 1.2) scaleDir = -1;
     if (scale <= 0.8) scaleDir = 1;
@@ -31,7 +41,7 @@ function animate() {
 
     logoDVD.style.left = `${x}px`;
     logoDVD.style.top = `${y}px`;
-    logoDVD.style.transform = `rotate(${angle}deg) scale(${scale})`;
+    logoDVD.style.transform = `translateZ(${z}px) rotateX(${angleX}deg) rotateY(${angleY}deg) rotateZ(${angle}deg) scale(${scale})`;
     logoDVD.style.filter = `hue-rotate(${hue}deg)`;
     body.style.backgroundColor = `hsl(${(hue + 180) % 360}, 50%, 10%)`;
 
